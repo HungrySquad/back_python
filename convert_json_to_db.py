503e2cb3512e02d72f5697cb8209575d8fa0445b
Meamzy <mumzyhydra@gmail.com>
2022-10-02 21:35:41 +0200

    Fixed indents for execute

--- convert_json_to_db.py
+++ convert_json_to_db.py
@@ -7,20 +7,18 @@ import sqlite3
 
 connection = sqlite3.connect('recipies.db')
 cursor = connection.cursor()
-cursor.execute(
-    """CREATE TABLE post (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
- VARCHAR(50),
-ingredients VARCHAR(3000),
-image VARCHAR(255),
-time VARCHAR(255),
-description VARCHAR(3000),
-source VARCHAR(255),
-nutrients VARCHAR(255),
-instructions VARCHAR(4000),
-servings VARCHAR(255)
-)"""
-)
+cursor.execute("""CREATE TABLE post (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,  
+	name VARCHAR(50), 
+	ingredients VARCHAR(3000), 
+	image VARCHAR(255), 
+	time VARCHAR(255), 
+	description VARCHAR(3000), 
+	source VARCHAR(255), 
+	nutrients VARCHAR(255), 
+	instructions VARCHAR(4000), 
+	servings VARCHAR(255)
+)""")
 with open('data_test.json', encoding="utf-8") as f:
     traffic = json.load(f)
     columns = [
