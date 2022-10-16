15e69b83b9899e5c1d31504f0fc8fc08cb6438a7
Mumzyhydra <mumzyhydra@gmail.com>
2022-10-16 20:15:13 +0200

    Fixed linting in recipies.py

--- app/routers/recipies.py
+++ app/routers/recipies.py
@@ -1,8 +1,16 @@
-from .. import models, schemas
-from ..database import engine, get_db
+# pylint: disable=invalid-name
+# pylint: disable=redefined-builtin
+# pylint: disable=relative-beyond-top-level
+"""
+Main routing module for API for the recipies with defined REST HTTP methods
+"""
+
+from typing import List
 from sqlalchemy.orm import Session
 from fastapi import Depends, Response, status, HTTPException, APIRouter
-from typing import List
+from .. import models, schemas
+from ..database import engine, get_db
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,7 +18,10 @@ router = APIRouter(prefix="/recipies", tags=["Recipies"])
 
 
 @router.get("/", response_model=List[schemas.RecipyResponse])
-def get_posts(db: Session = Depends(get_db)):
+def get_recipies(db: Session = Depends(get_db)):
+    """
+    Get all recipies
+    """
     recipies = db.query(models.Recipy).all()
     # cursor.execute("""SELECT * FROM recipies""")
     # recipies = cursor.fetchall()
@@ -21,6 +32,9 @@ def get_posts(db: Session = Depends(get_db)):
     "/", status_code=status.HTTP_201_CREATED, response_model=schemas.RecipyResponse
 )
 def create_recipy(recipy: schemas.RecipyCreate, db: Session = Depends(get_db)):
+    """
+    Creates a recipy from API call and adds it to the database
+    """
     # Does the same as above but quicker and cleaner
     new_recipy = models.Recipy(**recipy.dict())
     # Add new entry to the db
@@ -31,7 +45,10 @@ def create_recipy(recipy: schemas.RecipyCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=schemas.RecipyResponse)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_recipy(id: int, db: Session = Depends(get_db)):
+    """
+    Get single recipy by ID
+    """
     recipy = db.query(models.Recipy).filter(models.Recipy.id == id).first()
     if not recipy:
         raise HTTPException(
@@ -43,10 +60,13 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 @router.delete("/{id}")
 def remove_recipy(id: int, db: Session = Depends(get_db)):
+    """
+    Delete single recipy from database by ID
+    """
     recipy_query = db.query(models.Recipy).filter(models.Recipy.id == id)
     # conn.commit()
 
-    if recipy_query.first() == None:
+    if recipy_query.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Recipy with id: {id} does not exist",
@@ -58,11 +78,14 @@ def remove_recipy(id: int, db: Session = Depends(get_db)):
 
 @router.put("/{id}", response_model=schemas.RecipyResponse)
 def update_recipy(id: int, recipy: schemas.RecipyCreate, db: Session = Depends(get_db)):
+    """
+    Update single recipy by id using the passed data from API
+    """
 
     recipy_query = db.query(models.Recipy).filter(models.Recipy.id == id)
     updated_recipy = recipy_query.first()
 
-    if updated_recipy == None:
+    if updated_recipy is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Recipy with the id: {id} wasn't found.",
